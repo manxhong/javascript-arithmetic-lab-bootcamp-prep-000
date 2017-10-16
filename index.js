c6d83615fadc3a6ecfa5fe58c3bb23e02bd42388
Man Hong Lee <manhong323@gmail.com>
2017-10-16 21:50:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ var floatConverter = parseFloat('2.2222');
 function add(a,b){
   return a+b;
 }
+
+function subtract(a,b){
+  return b - a;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(n){
+  return n++;
+}
+
+function dec(n){
+  return n--;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
